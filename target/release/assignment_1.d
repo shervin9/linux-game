e3a7745c68b34f943eb6ff0109cbb58b239319a4
root <root@srv5692238634.bitcommand.net>
2025-01-07 19:59:27 +0000

    Update .gitignore and add target directory

---
+++ target/release/assignment_1.d
@@ -0,0 +1,1 @@
+/root/linux-game/target/release/assignment_1: /root/linux-game/src/main.rs
---
+++ target/release/deps/assignment_1-6e21cf5202257070.d
@@ -0,0 +1,5 @@
+/root/linux-game/target/release/deps/assignment_1-6e21cf5202257070: src/main.rs
+
+/root/linux-game/target/release/deps/assignment_1-6e21cf5202257070.d: src/main.rs
+
+src/main.rs:
